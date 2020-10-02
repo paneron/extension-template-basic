9d52cc89f5e6c44e81fbac260af18e678bc54ea5
Anton Strogonoff <anton@strogonoff.name>
2020-10-02 17:54:01 +0900

    Initial commit

---
+++ src/RepoView.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import { H4 } from '@blueprintjs/core';
+import { RepositoryViewProps } from '@riboseinc/paneron-plugin-kit/types';
+
+
+export const RepositoryView: React.FC<RepositoryViewProps> =
+function () {
+  const [busy, setBusy] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setBusy(false);
+    }, 5000);
+  }, []);
+
+  return (
+    <>
+      <H4>{busy ? "Loading…" : "Hello!"}</H4>
+    </>
+  );
+};
